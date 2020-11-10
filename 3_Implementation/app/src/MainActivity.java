9605620136be697e21ecc02819060f683efa8cad
99002454 <71604168+99002454@users.noreply.github.com>
2020-11-10 12:46:17 +0530

    Add files via upload
    Unit converter part is added by Bindu

---
+++ 3_Implementation/app/src/MainActivity.java
@@ -0,0 +1,39 @@
+package com.LNT.scientificcalc_unitc;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> applicationName = new ArrayList<>(Arrays.asList("Scientific Calc","Unit Converter","BMI Calculator"));
+
+    ArrayList<Integer> imageArray = new ArrayList<Integer>(Arrays.asList(R.drawable.image,R.drawable.download,R.drawable.bmi));
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView=(RecyclerView)findViewById(R.id.recyclerView);
+
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        CustomAdapter customAdapter = new CustomAdapter(MainActivity.this,applicationName, imageArray);
+        recyclerView.setAdapter(customAdapter);
+
+    }
+
+
+}
